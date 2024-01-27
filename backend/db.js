f935d5153a536cfed4e5303fe88ae30b1ec70959
sahilkuthe <sahilkkuthe2004@gmail.com>
2024-01-27 23:16:24 +0530

    mongoose schema for user

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,20 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose, model } from "mongoose";
 require('dotenv').config();
 mongoose.connect(process.env.MONGO_URL);
+
+//create a monggose schema
+
+const userSchema = new mongoose.Schema({
+    firstname: String,
+    lastName: String,
+    password: String,
+
+})
+
+
+//create a model of the schema
+const User = mongoose.model('User', userSchema);
+//export the model
+module.exports = {
+    User
+}
