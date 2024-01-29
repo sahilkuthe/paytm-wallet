fa4e471e74fe68e5f77dea515ff48ccd4f2e1f76
sahilkuthe <sahilkkuthe2004@gmail.com>
2024-01-29 19:19:31 +0530

    transfer route

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -14,6 +14,54 @@ router.get("/", async (req, res) => {
     res.json({
         balance: account.balance
     })
+});
+
+//endpoint for user to transfer money to another account
+router.post("/transfer", authMiddleware, async (req, res) => {
+    const {amount, to} = req.body;      //get the amount and userid of the account to send money
+
+    const account = await Account.findONe({       //this will get us users account
+        userId: req.userId
+    })
+
+    if(account.balance < amount){
+        res.status(400).json({
+            message: "Insufficient funds"
+        })
+    }
+    
+    const toAccount = await Account.findONe({       //to check the validity of account
+        userId: to
+    })
+
+    if(!toAccount){
+        res.status(400).json({
+            message: "Invalid account"
+        })
+    }
+
+    await Account.updateOne({
+        userId: req.userId
+    }, {
+        $inc: {
+            balance: -amount
+        }
+        
+    })
+
+    
+    await Account.updateOne({
+        userId: to
+    }, {
+        $inc: {
+            balance: amount
+        }
+        
+    })
+
+    res.json({
+        message: "Transfer successfull!"
+    })
 })
 
 
